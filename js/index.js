012f811e57f831dc0b1386584e2ebe4217768559
israt <isratjahanpanna87@gmail.com>
2023-09-03 04:05:15 +0600

    card tab done1

--- js/index.js
+++ js/index.js
@@ -16,13 +16,13 @@ console.log(data.data);
 const handleNodeBlogs = async (categoryId) => {
     const response = await fetch (`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const cardContainer = document.getElementById('card-container')
-    cardContainer.innerHTML="";
+    cardContainer.innerHTML = "";
     const data = await response.json();
-    data.data?.forEach((videos)=> {
+    data.data.forEach((videos)=> {
         console.log(videos);
  const div = document.createElement("div")
         div.innerHTML= `  
-        <div class="card card-compact  bg-base-100 shadow-xl h-[360px]">
+        <div class="card card-compact  bg-base-100 shadow-xl h-[370px]">
         <img class="h-52" src=${videos.thumbnail} alt="">
                     <div class="card-body">
                    <div class="flex flex-row gap-3">
@@ -31,7 +31,7 @@ const handleNodeBlogs = async (categoryId) => {
                         <div class="flex flex-row">
                             <h2 class="card-title font-bold">${videos.title}</h2>
                             
-                            <img class="h-6 w-6" src=${videos.others?.verified } alt="">
+                            <img class="h-6 w-6" src=${videos.others.verified } alt="">
                         </div>
                         <h4 class="text-gray-400">${videos.authors[0].profile_name} alt</h4>
                         <p class="text-gray-400">${videos.others.views} </p>
@@ -49,4 +49,4 @@ const handleNodeBlogs = async (categoryId) => {
 };
 
 handleCategory();
-handleNodeBlogs('1000');
+handleNodeBlogs("1000")
